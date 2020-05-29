f8272077e09ed3cca3f1efdf6dd2e9d53f8e5782
Prem Patel <prempatel40p@outlook.com>
2020-05-29 13:14:08 +0530

    initial commit. Required dependencies added.

---
+++ app/src/main/java/com/pr656d/userslist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pr656d.userslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
